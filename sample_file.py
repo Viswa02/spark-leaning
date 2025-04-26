03dd736c6eba38e467b4f1f45a502b80054148c5
Viswa02 <Viswa02>
2025-04-26 04:09:15 +0000

    dummy table created and loaded

--- sample_file.py
+++ sample_file.py
@@ -1,8 +1,4 @@
 # Databricks notebook source
-print('Hi')
-
-# COMMAND ----------
-
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
@@ -12,28 +8,16 @@ spark = SparkSession.builder.appName('databricks-learning').getOrCreate()
 
 # COMMAND ----------
 
-df = spark.range(1, 15000000000, 1, 10)
-
+spark.sql('create catalog if not exists databricks_learning')
+spark.sql('create database if not exists databricks_learning.dummy')
+spark.sql('use databricks_learning.dummy')
 
 # COMMAND ----------
 
+df = spark.range(1, 15000, 1, 10)
 new_df = df.withColumn('temp', F.rand())
-
-# COMMAND ----------
-
 new_df.write.mode('overwrite').saveAsTable('temp')
 
-# COMMAND ----------
-
-spark.sql('create catalog if not exists databricks_learning')
-
-# COMMAND ----------
-
-spark.sql('create database databricks_learning.dummy')
-
-# COMMAND ----------
-
-spark.sql('use databricks_learning.dummy')
 
 # COMMAND ----------
 
